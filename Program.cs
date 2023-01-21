d69da9afda531a36f642271a19947042bfb47d49
anu1820 <anurajpatil35@gmail.com>
2023-01-21 08:05:17 +0530

    Method calling

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using MoodAnalyserAssingment;
+
+namespace ModeAnalyzerAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message;
+            Random random = new Random();
+            int mood = random.Next(2);
+
+            if (mood == 1)
+            {
+                message = "Happy";
+            }
+            else
+            {
+                message = "Sad";
+
+            }
+            Console.WriteLine(MoodAnalyzer.analyseMood(message));
+        }
+
+
+    }
+    
+
+}
